71cfaea4a32d34aa8d3ad1289e8fdf1f3f2ea4db
Marc "Jakon" Lapeña <jakoncoach@gmail.com>
2025-03-18 16:34:33 +0100

    .sh's

---
+++ CompileALL.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Compiling Server..."
+javac src/Server/Server.java
+
+echo "Compiling Servant..."
+javac src/Server/Servant.java
+
+echo "Compiling Client..."
+javac src/Client/Client.java
+
+echo "Compiling Listener..."
+javac src/Client/ClientListener.java
+
+echo "Compiling MessageRMI..."
+javac src/MessageRMI.java
---
+++ ExecuteClient.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+@echo off
+set /p SERVER_IP=Enter the server IP (default localhost):
+if "%SERVER_IP%"=="" set SERVER_IP=localhost
+
+echo Connecting to server at %SERVER_IP%...
+java -cp . src/Client/Client %SERVER_IP%
+pause
