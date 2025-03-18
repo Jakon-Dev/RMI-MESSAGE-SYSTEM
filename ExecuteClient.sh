4f28fa311b5e631b2dd3585a66a13d340a6f2da3
Marc "Jakon" Lapeña <jakoncoach@gmail.com>
2025-03-18 20:41:00 +0100

    Update ExecuteClient.sh

--- ExecuteClient.sh
+++ ExecuteClient.sh
@@ -3,5 +3,9 @@
 read -p "Enter the server IP (default localhost): " SERVER_IP
 SERVER_IP=${SERVER_IP:-localhost}  # Si el usuario no introduce nada, usa localhost
 
-echo "Connecting to server at $SERVER_IP..."
-java -cp . src.Client.Client $SERVER_IP
+read -p "Enter the server IP (default localhost): " CLIENTIP
+CLIENTIP=${CLIENTIP:-localhost}  # Si el usuario no introduce nada, usa localhost
+
+
+echo "Connecting to server at $SERVER_IP from $CLIENTIP..."
+java -Djava.rmi.server.hostname=$CLIENTIP -cp . src.Client.Client $SERVER_IP
